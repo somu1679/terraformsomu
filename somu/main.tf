e096c9dfa6c919b148dca73d1d8281e12f9359eb
root <root@ip-172-31-45-23.ap-south-1.compute.internal>
2021-12-03 17:18:39 +0000

    terraform ec2 and vpcs

---
+++ somu/main.tf
@@ -0,0 +1,108 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+terraform {
+  required_version = ">= 0.14" #Forcing which version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = ">= 3.0.0" #Forcing which version of plugin needs to be used.
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+	Owner = "SOMASEKHAR"
+	environment = "${var.environment}"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "${var.IGW_name}"
+    }
+}
+
+resource "aws_subnet" "subnet1-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet1_cidr}"
+    availability_zone = "ap-south-1a"
+
+    tags = {
+        Name = "${var.public_subnet1_name}"
+    }
+}
+
+resource "aws_subnet" "subnet2-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet2_cidr}"
+    availability_zone = "ap-south-1b"
+
+    tags = {
+        Name = "${var.public_subnet2_name}"
+    }
+}
+
+resource "aws_subnet" "subnet3-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet3_cidr}"
+    availability_zone = "ap-south-1c"
+
+    tags = {
+        Name = "${var.public_subnet3_name}"
+    }
+	
+}
+
+
+resource "aws_route_table" "terraform-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags = {
+        Name = "${var.Main_Routing_Table}"
+    }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    route_table_id = "${aws_route_table.terraform-public.id}"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.default.id}"
+}
+
+resource "aws_instance" "web-1" {
+     #ami = var.imagename
+     ami = "ami-052cef05d01020f1d"
+     #ami = "${data.aws_ami.my_ami.id}"
+     availability_zone = "ap-south-1a"
+     instance_type = "t2.micro"
+     key_name = "somu-aws"
+     subnet_id = "${aws_subnet.subnet1-public.id}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-1"
+        #Env = "Prod"
+        #Owner = "Sree"
+ 	#CostCenter = "ABCD"
+     }
+}
+
---
+++ somu/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+		ap-south-1 = "ami-07a4db51302058e77" # ubuntu 14.04 LTS
+    }
+}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  default = {
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
+}
+
